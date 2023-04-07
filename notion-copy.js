2a183f504e811dc7c76e360a395f99d503dcc4ac
William Hunter <wlhunter00@gmail.com>
2023-04-07 12:35:28 -0400

    progress

--- notion-copy.js
+++ notion-copy.js
@@ -6,61 +6,80 @@ const notion = new Client({ auth: process.env.NOTION_SECRET })
 
 // Query the artists notion id from the database
 const artistIDQuery = await notion.databases.query({
-    database_id: "8c53db8170764a0480cf9bcab3b5233e"
+    database_id: "854650ae83f34073b04819c0ecf6378b"
     ,
     filter: {
         and: [
             {
-                property: 'Name',
+                property: 'Artist Name',
                 rich_text:
                 {
-                    contains: "Xer0x"
+                    contains: "0010"
                 }
             }
         ]
     }
 });
 
-async function updateNotionPage(id, address, description, socials) {
+async function updateNotionPage(id, pfp, banner, name) {
     try {
-        const response = await notion.pages.update({
-            page_id: id,
-            properties: {
-                'Sign-In Address (1)': {
-                    "rich_text": [
-                        {
-                            "type": "text",
-                            "text": {
-                                "content": address
-                            }
-                        },
-                    ]
+        if (pfp && banner) {
+            const response = await notion.pages.update({
+                page_id: id,
+                properties: {
+                    'PFP Image (1)': {
+                        "files":
+                            [
+                                {
+                                    "type": "external",
+                                    "name": `${name}_pfp`,
+                                    external: {
+                                        "url": pfp,
+                                    },
+                                }
+                            ]
+
+                    },
+                    'Banner Image (1)': {
+                        "files":
+                            [
+                                {
+                                    "type": "external",
+                                    "name": `${name}_banner`,
+                                    external: {
+                                        "url": banner,
+                                    },
+                                }
+                            ]
+
+                    }
                 },
-                'Artist Description (1)': {
-                    "rich_text": [
-                        {
-                            "type": "text",
-                            "text": {
-                                "content": description
-                            }
-                        },
-                    ]
+            })
+            console.log("Success! Entry updated", name)
+        }
+        else {
+            const response = await notion.pages.update({
+                page_id: id,
+                properties: {
+                    'PFP Image (1)': {
+                        "files":
+                            [
+                                {
+                                    "name": `${name}_pfp`,
+                                    type: "file",
+                                    file: {
+                                        "url": pfp,
+                                    },
+                                }
+                            ]
+
+                    }
                 },
-                'Socials (1)': {
-                    "rich_text": [
-                        {
-                            "type": "text",
-                            "text": {
-                                "content": socials
-                            }
-                        },
-                    ]
-                }
-            },
-        })
-        console.log("Success! Entry updated", id)
+            })
+            console.log("Success! Entry updated", name)
+        }
     } catch (error) {
-        console.log("Error found when adding", id, "to Notion!");
+        console.log("Error found when adding", name, "to Notion!");
         console.error(error.body);
         // Pushes error object if there is an issue 
     }
@@ -68,15 +87,40 @@ async function updateNotionPage(id, address, description, socials) {
 
 
 artistIDQuery.results.forEach(artist => {
-    const artistID = artist.id;
-    // address, description, PFP, banner image, socials
-    const address = artist.properties["Sign-In Address"].rollup.array[0] ? artist.properties["Sign-In Address"].rollup.array[0].rich_text[0].plain_text : "";
-    const description = (artist.properties['Artist Description'].rollup.array[0] && artist.properties['Artist Description'].rollup.array[0].rich_text[0]) ? artist.properties['Artist Description'].rollup.array[0].rich_text[0].plain_text : "";
-    const socials = (artist.properties['Socials'].rollup.array[0] && artist.properties['Socials'].rollup.array[0].rich_text[0]) ? artist.properties['Socials'].rollup.array[0].rich_text[0].plain_text : "";
-    console.log(address, description, socials)
-    // console.log(artist.properties)
-    if (address)
-        updateNotionPage(artistID, address, description, socials);
+    let pfp = undefined;
+    let bannerImage = undefined;
+
+    if (artist.properties["PFP"].files[0]) {
+        if (artist.properties["PFP"].files[0].external)
+            pfp = artist.properties["PFP"].files[0].external.url
+        else if (artist.properties["PFP"].files[0].file) {
+            console.log(artist.properties["PFP"].files[0])
+            pfp = artist.properties["PFP"].files[0].file.url
+        }
+    }
+
+    if (artist.properties["Banner Image"].files[0]) {
+        if (artist.properties["Banner Image"].files[0].external) {
+            bannerImage = artist.properties["Banner Image"].files[0].external.url
+        }
+        else if (artist.properties["Banner Image"].files[0].file)
+            bannerImage = artist.properties["Banner Image"].files[0].file.url
+    }
+    let id = artist.properties['👨‍👩‍👧‍👦 Escher Artist Database'].relation[0].id;
+
+    const name = artist.properties["Artist Name"].title[0].text.content;
+
+    console.log(id, pfp, bannerImage, name)
+
+    // const artistID = artist.id;
+    // // address, description, PFP, banner image, socials
+    // const address = artist.properties["Sign-In Address"].rollup.array[0] ? artist.properties["Sign-In Address"].rollup.array[0].rich_text[0].plain_text : "";
+    // const description = (artist.properties['Artist Description'].rollup.array[0] && artist.properties['Artist Description'].rollup.array[0].rich_text[0]) ? artist.properties['Artist Description'].rollup.array[0].rich_text[0].plain_text : "";
+    // const socials = (artist.properties['Socials'].rollup.array[0] && artist.properties['Socials'].rollup.array[0].rich_text[0]) ? artist.properties['Socials'].rollup.array[0].rich_text[0].plain_text : "";
+    // console.log(address, description, socials)
+    // // console.log(artist.properties)
+    if (pfp)
+        updateNotionPage(id, pfp, bannerImage, name);
     console.log("------")
 });
 
